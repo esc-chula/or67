df9d153c532872ef3b62d27945230017c5e1f246
HotPaotatoes <113345557+ChanatpakornDev@users.noreply.github.com>
2024-07-02 18:41:43 +0700

    feat: types

--- apps/or67/src/types/student.ts
+++ apps/or67/src/types/student.ts
@@ -1,11 +1,11 @@
-import { Program } from "./program"
+import { Program } from './program';
 
-export interface Student {
-	"index": number,
-	"id": string,
-	"name": {
-		"th": string,
-		"en": string
-	},
-	"program": Program
+export interface IStudent {
+    index: number;
+    id: string;
+    name: {
+        th: string;
+        en: string;
+    };
+    program: Program;
 }
