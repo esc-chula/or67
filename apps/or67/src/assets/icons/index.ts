572cb875b45bd863572b10e6220a7a37240c3c2d
shalluv <leon555thai@gmail.com>
2024-07-08 22:14:29 +0700

    feat: layout and nav

---
+++ apps/or67/src/assets/icons/index.ts
@@ -0,0 +1,9 @@
+import { BookIcon } from "./book";
+import { CalendarIcon } from "./calendar";
+import { EditIcon } from "./edit";
+import { FinderIcon } from "./finder";
+import { ProfileIcon } from "./profile";
+import { SignoutIcon } from "./signout";
+import { TableIcon } from "./table";
+
+export { BookIcon, CalendarIcon, EditIcon, FinderIcon, ProfileIcon, SignoutIcon, TableIcon };
---
+++ apps/or67/src/components/nav/index.ts
@@ -0,0 +1,4 @@
+import { Top } from './top';
+import { Bottom } from './bottom';
+
+export { Top as NavTop, Bottom as NavBottom };
