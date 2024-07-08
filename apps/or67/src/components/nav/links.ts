142f6e5db8c14bec389285b658a315c6f14c06c7
shalluv <leon555thai@gmail.com>
2024-07-08 23:46:21 +0700

    refactor: refactor redundant codes

--- apps/or67/src/components/nav/links.ts
+++ apps/or67/src/components/nav/links.ts
@@ -1,54 +1,54 @@
 import {
-    CalendarIcon,
-    EditIcon,
-    FinderIcon,
-    ProfileIcon,
-    TableIcon,
+	CalendarIcon,
+	EditIcon,
+	FinderIcon,
+	ProfileIcon,
+	TableIcon,
 } from '@/assets/icons';
 import type { SvgIconProps } from '@/assets/icons/type';
 
 export interface Link {
-    href: string;
-    label: string;
-    icon: (props: SvgIconProps) => JSX.Element;
-    needFill: boolean;
-    needStroke: boolean;
+	href: string;
+	label: string;
+	icon: (props: SvgIconProps) => JSX.Element;
+	needFill: boolean;
+	needStroke: boolean;
 }
 
 export const links: Link[] = [
-    {
-        href: '/profile',
-        label: 'ข้อมูลนิสิต',
-        icon: ProfileIcon,
-        needFill: false,
-        needStroke: true,
-    },
-    {
-        href: '/subjects',
-        label: 'รายวิชา',
-        icon: EditIcon,
-        needFill: true,
-        needStroke: true,
-    },
-    {
-        href: '/schedule',
-        label: 'ตารางเรียน',
-        icon: TableIcon,
-        needFill: true,
-        needStroke: true,
-    },
-    {
-        href: '/calendar',
-        label: 'ปฏิทินคณะ',
-        icon: CalendarIcon,
-        needFill: true,
-        needStroke: true,
-    },
-    {
-        href: '/makefriends',
-        label: 'หาเพื่อน',
-        icon: FinderIcon,
-        needFill: false,
-        needStroke: true,
-    },
+	{
+		href: '/profile',
+		label: 'ข้อมูลนิสิต',
+		icon: ProfileIcon,
+		needFill: false,
+		needStroke: true,
+	},
+	{
+		href: '/subjects',
+		label: 'รายวิชา',
+		icon: EditIcon,
+		needFill: true,
+		needStroke: true,
+	},
+	{
+		href: '/schedule',
+		label: 'ตารางเรียน',
+		icon: TableIcon,
+		needFill: true,
+		needStroke: true,
+	},
+	{
+		href: '/calendar',
+		label: 'ปฏิทินคณะ',
+		icon: CalendarIcon,
+		needFill: true,
+		needStroke: true,
+	},
+	{
+		href: '/makefriends',
+		label: 'หาเพื่อน',
+		icon: FinderIcon,
+		needFill: false,
+		needStroke: true,
+	},
 ];
