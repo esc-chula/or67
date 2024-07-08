f2a7c43a1d162dc6c4bbab621c3de691a657758b
shalluv <leon555thai@gmail.com>
2024-07-08 23:46:21 +0700

    refactor: refactor redundant codes

---
+++ apps/or67/src/components/nav/links.ts
@@ -0,0 +1,54 @@
+import {
+	CalendarIcon,
+	EditIcon,
+	FinderIcon,
+	ProfileIcon,
+	TableIcon,
+} from '@/assets/icons';
+import type { SvgIconProps } from '@/assets/icons/type';
+
+export interface Link {
+	href: string;
+	label: string;
+	icon: (props: SvgIconProps) => JSX.Element;
+	needFill: boolean;
+	needStroke: boolean;
+}
+
+export const links: Link[] = [
+	{
+		href: '/',
+		label: 'ข้อมูลนิสิต',
+		icon: ProfileIcon,
+		needFill: false,
+		needStroke: true,
+	},
+	{
+		href: '/subjects',
+		label: 'วิชาที่ต้องลงทะเบียน',
+		icon: EditIcon,
+		needFill: true,
+		needStroke: true,
+	},
+	{
+		href: '/schedule',
+		label: 'ตารางเรียน',
+		icon: TableIcon,
+		needFill: true,
+		needStroke: true,
+	},
+	{
+		href: '/calendar',
+		label: 'ปฏิทินคณะ',
+		icon: CalendarIcon,
+		needFill: true,
+		needStroke: true,
+	},
+	{
+		href: '/makefriends',
+		label: 'หาเพื่อน',
+		icon: FinderIcon,
+		needFill: false,
+		needStroke: true,
+	},
+]
