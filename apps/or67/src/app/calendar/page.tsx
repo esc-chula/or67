d8d746625da08099716dc64e92b7124f46c5265f
HotPaotatoes <113345557+ChanatpakornDev@users.noreply.github.com>
2024-07-10 04:44:14 +0700

    feat: calendar no dot index

--- apps/or67/src/app/calendar/page.tsx
+++ apps/or67/src/app/calendar/page.tsx
@@ -1,67 +1,122 @@
+'use client';
 import Link from 'next/link';
 import Image, { StaticImageData } from 'next/image';
-import { Carousel, CarouselContent, CarouselItem } from '@ui/components/ui/carousel';
+import { useState, useEffect } from 'react';
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    type CarouselApi,
+} from '@ui/components/ui/carousel';
 import { FacebookIcon } from '@/assets/icons/facebook';
+import { InstagramIcon } from '@/assets/icons/instagram';
 import Calendar_1 from '@/assets/images/calendar/calendar_1.png';
 import Calendar_2 from '@/assets/images/calendar/calendar_2.png';
+import CalendarCarousel_1 from '@/assets/images/calendar/calendarCarousel_1.png';
+import CalendarCarousel_2 from '@/assets/images/calendar/calendarCarousel_2.png';
+import CalendarCarousel_3 from '@/assets/images/calendar/calendarCarousel_3.png';
 
 const calendarCarousel: { src: StaticImageData }[] = [
     {
-        src: Calendar_1
+        src: CalendarCarousel_1,
     },
     {
-        src: Calendar_1
+        src: CalendarCarousel_2,
     },
     {
-        src: Calendar_1
-    }
+        src: CalendarCarousel_3,
+    },
 ];
 
 export default function Page(): JSX.Element {
+    const [api, setApi] = useState<CarouselApi>();
+    const [current, setCurrent] = useState<number>(0);
+
+    useEffect(() => {
+        if (!api) {
+            return;
+        }
+
+        setCurrent(api.selectedScrollSnap() + 1);
+
+        api.on('select', () => {
+            setCurrent(api.selectedScrollSnap() + 1);
+        });
+    }, [api]);
+
     return (
         <main className=''>
-            <h2 className='text-center text-2xl lg:text-4xl font-semibold'>ปฏิทินคณะ</h2>
-            <div className='flex flex-col items-center'>
-                <article className='flex flex-col justify-center'>
-                    <Carousel orientation='horizontal'>
-                        <CarouselContent>
-                            {
-                                calendarCarousel.map((item, index) => (
-                                    <CarouselItem key={index}>
-                                        <Image src={item.src} width={393} height={393} alt="_blank"/>
+            <h2 className='mb-12 text-center text-2xl font-semibold lg:text-4xl'>
+                ปฏิทินคณะ
+            </h2>
+            <div className='grid grid-cols-1 gap-5 lg:grid-cols-3 w-full'>
+                <article className='text-base '>
+                    <div className='z-0'>
+                        <Carousel orientation='horizontal' setApi={setApi}>
+                            <CarouselContent>
+                                {calendarCarousel.map((item, index) => (
+                                    <CarouselItem key={index}
+                                    className=''>
+                                        <Image
+                                            src={item.src}
+                                            alt='_blank'
+                                        />
                                     </CarouselItem>
-                                ))
-                            }
-                        </CarouselContent>
-                    </Carousel>
-                    <p>
-                    📍 ขอเชิญชวนให้นิสิตใหม่ Intania 108 ทุกคนเข้ากลุ่ม Facebook และ LINE OpenChat เพื่อติดตามข่าวสาร ประกาศ และกิจกรรมต่าง ๆ ภายในวิศวฯ จุฬาฯ กันนะ<br/><br/>🚨 ชาว Intania 108 ทุกคนอย่าลืมกรอกแบบฟอร์มนิสิตใหม่ก่อนที่จะขอเข้ากลุ่มกันด้วยนะ ✨<br/>👉 
-                    <a href="https://intania.link/108-entry-form" className='underline'>https://intania.link/108-entry-form</a> 
+                                ))}
+                            </CarouselContent>
+                        </Carousel>
+                    </div>
+                    <p className='mt-4 mb-12 px-2 text-start'>
+                        📍 ขอเชิญชวนให้นิสิตใหม่ Intania 108 ทุกคนเข้ากลุ่ม
+                        Facebook และ LINE OpenChat เพื่อติดตามข่าวสาร ประกาศ
+                        และกิจกรรมต่าง ๆ ภายในวิศวฯ จุฬาฯ กันนะ
+                        <br />
+                        <br />
+                        🚨 ชาว Intania 108
+                        ทุกคนอย่าลืมกรอกแบบฟอร์มนิสิตใหม่ก่อนที่จะขอเข้ากลุ่มกันด้วยนะ
+                        ✨<br />
+                        👉
+                        <a
+                            href='https://intania.link/108-entry-form'
+                            className='underline'
+                        >
+                            https://intania.link/108-entry-form
+                        </a>
                     </p>
                 </article>
-                <article>
-                    <Image src={Calendar_1} width={393} height={393} alt="_blank"/>
-                    <p>
-                    📣 กำหนดการกิจกรรมสำหรับนิสิตวิศวฯ ⚙️🗓️<br/><br/>ชาว Intania 108 ทุกคนจะได้เจอกับกิจกรรมต่างมากมายและพบเพื่อนทั้งในและนอกคณะ บอกเลยว่าห้ามพลาดทุกกิจกรรมเลย !
+                <article className='text-base w-full h-full items-center'>
+                    <Image
+                        src={Calendar_1}
+                        alt='_blank'
+                    />
+                    <p className='mt-4 mb-12 px-2 text-start'>
+                        📣 กำหนดการกิจกรรมสำหรับนิสิตวิศวฯ ⚙️🗓️
+                        <br />
+                        <br />
+                        ชาว Intania 108
+                        ทุกคนจะได้เจอกับกิจกรรมต่างมากมายและพบเพื่อนทั้งในและนอกคณะ
+                        บอกเลยว่าห้ามพลาดทุกกิจกรรมเลย !
                     </p>
                 </article>
-                <article>
-                    <Image src={Calendar_2} width={393} height={393} alt="_blank"/>
-                    <p>
-                        📣 กำหนดการการลงทะเบียนนิสิตใหม่ ภาคการศึกษาต้น ปีการศึกษา 2567 ⚙️📚 [รหัส 67]
+                <article className='text-base'>
+                    <Image
+                        src={Calendar_2}
+                        alt='_blank'
+                    />
+                    <p className='mt-4 mb-12 px-2 text-start'>
+                        📣 กำหนดการการลงทะเบียนนิสิตใหม่ ภาคการศึกษาต้น
+                        ปีการศึกษา 2567 ⚙️📚 [รหัส 67]
                     </p>
                 </article>
             </div>
-            <div className='flex flex-col items-center'>
+            <div className='flex flex-col items-center lg:m-20'>
                 <h3>ติดตามข่าวสารเพิ่มเติมได้ที่</h3>
-                <div className='flex flex-row gap-x-4'>
-                    <Link href={
-                        "https://www.facebook.com/escchula"
-                    }>
-                        <FacebookIcon width={52} height={52}/>
+                <div className='m-4 flex flex-row gap-x-5'>
+                    <Link href={'https://www.facebook.com/escchula'}>
+                        <FacebookIcon width={52} height={52} />
                     </Link>
-                    <Link href={"https://www.instagram.com/escchula/?hl=en"}>
-                        <FacebookIcon width={52} height={52}/>
+                    <Link href={'https://www.instagram.com/escchula/?hl=en'}>
+                        <InstagramIcon width={52} height={52} />
                     </Link>
                 </div>
             </div>
