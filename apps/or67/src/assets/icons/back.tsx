3f54208452febd4c87658124e8eceb583d8862e5
shalluv <leon555thai@gmail.com>
2024-07-11 00:33:22 +0700

    format

--- apps/or67/src/assets/icons/back.tsx
+++ apps/or67/src/assets/icons/back.tsx
@@ -10,8 +10,13 @@ export function BackIcon({ className, fill }: SvgIconProps): JSX.Element {
             width='24'
             xmlns='http://www.w3.org/2000/svg'
         >
-            <path d="M8 5L0.999938 12.0001L8 19.0001" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-            fill={fill}
+            <path
+                d='M8 5L0.999938 12.0001L8 19.0001'
+                stroke='black'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                fill={fill}
             />
         </svg>
     );
