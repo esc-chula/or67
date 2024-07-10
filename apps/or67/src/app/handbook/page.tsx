ea080063ccf7c3471ff0532eeadc94a2f60664bc
shalluv <leon555thai@gmail.com>
2024-07-11 01:47:25 +0700

    refactor: handbook

--- apps/or67/src/app/handbook/page.tsx
+++ apps/or67/src/app/handbook/page.tsx
@@ -1,51 +1,56 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { ESCLogoWithText } from '@ui/components/esc';
 import { BackIcon } from '@/assets/icons/back';
-import Handbook1 from '@/assets/images/handbook/handbook1.png';
 import Handbook2 from '@/assets/images/handbook/handbook2.png';
 import Handbook3 from '@/assets/images/handbook/handbook3.png';
 import Handbook4 from '@/assets/images/handbook/handbook4.png';
 import Handbook5 from '@/assets/images/handbook/handbook5.png';
 
+function Header({ children }: { children: string }): JSX.Element {
+    return (
+        <div className='flex h-fit w-full gap-3'>
+            <div className='bg-primary w-fit'>
+                <h2 className='text-primary-foreground px-5 py-3 text-lg font-bold md:text-xl lg:px-10 lg:py-5 lg:text-2xl'>
+                    {children}
+                </h2>
+            </div>
+            <div className='bg-primary w-3 lg:w-6' />
+        </div>
+    );
+}
+
+function Link({ href }: { href: string }): JSX.Element {
+    return (
+        <NextLink
+            className='text-primary font-semibold underline'
+            href={href}
+            rel='noreferrer noopener'
+            target='_blank'
+        >
+            {href}
+        </NextLink>
+    );
+}
+
 export default function Handbook(): JSX.Element {
     return (
-        <main className='text-medium flex flex-col justify-center space-y-16 pb-16 font-medium lg:pb-32 lg:text-2xl'>
-            <Link href='/'>
-                <BackIcon className='lg:hidde stroke-primary size-7 items-start' />
-            </Link>
-            <div className='flex w-full max-w-screen-lg flex-col items-center space-y-8 lg:space-y-0 lg:pt-0'>
-                <div className='lg:hidden'>
-                    <ESCLogoWithText />
-                </div>
-                <h1 className='px-10 text-center text-2xl font-bold lg:text-4xl'>
+        <main className='mt-24 flex flex-col justify-center gap-8 pb-12 text-sm md:text-lg lg:mt-0 lg:pb-28 lg:text-2xl'>
+            <NextLink className='absolute top-8' href='/'>
+                <BackIcon className='stroke-primary size-7 lg:hidden' />
+            </NextLink>
+            <div className='mb-8 flex w-full flex-col items-center gap-6'>
+                <ESCLogoWithText className='lg:hidden' />
+                <h1 className='px-10 text-center text-2xl font-semibold lg:text-4xl'>
                     คู่มือการลงทะเบียนเรียนใน REG CHULA
                 </h1>
             </div>
-            <div className='w-full space-y-8 lg:space-y-10'>
-                <div className='flex h-fit w-full flex-row space-x-3'>
-                    <div
-                        className='w-fit'
-                        style={{ backgroundColor: '#821923' }}
-                    >
-                        <h2 className='px-5 py-2 text-lg font-bold text-white lg:px-10 lg:py-5 lg:text-2xl'>
-                            การตรวจสอบตารางเรียน
-                        </h2>
-                    </div>
-                    <div
-                        className='w-3'
-                        style={{ backgroundColor: '#821923' }}
-                    />
-                </div>
-                <ol className='list-decimal space-y-2 px-10 lg:px-24'>
+            <div className='flex w-full flex-col gap-6'>
+                <Header>การตรวจสอบตารางเรียน</Header>
+                <ol className='flex list-inside list-decimal flex-col gap-2 px-5'>
                     <li>
                         เข้าเว็บไซต์{' '}
-                        <Link
-                            className='underline'
-                            href='https://or67.chula.engineering'
-                        >
-                            https://or67.chula.engineering
-                        </Link>{' '}
+                        <Link href='https://or67.chula.engineering' />{' '}
                         เพื่อตรวจสอบตารางเรียน
                     </li>
                     <li>
@@ -62,139 +67,80 @@ export default function Handbook(): JSX.Element {
                         นิสิตสามารถกดปุ่ม &quot;ดูตารางเรียนใน CU GET REG&quot;{' '}
                         <br />
                         เพื่อสามารถดูตารางเรียนของตนเองในเว็บไซต์{' '}
-                        <Link
-                            className=' font-bold underline'
-                            href='https://cugetreg.com'
-                            style={{ color: '#821923' }}
-                        >
-                            https://cugetreg.com
-                        </Link>{' '}
-                        ได้
+                        <Link href='https://cugetreg.com' /> ได้
                     </li>
                 </ol>
             </div>
-            <div className='w-full space-y-8 lg:space-y-10'>
-                <div className='flex h-fit w-full flex-row space-x-3'>
-                    <div
-                        className='w-fit'
-                        style={{ backgroundColor: '#821923' }}
-                    >
-                        <h2 className='px-5 py-2 text-lg font-bold text-white lg:px-10 lg:py-5 lg:text-2xl'>
-                            ขั้นตอนการเข้าสู่ระบบลงทะเบียน
-                        </h2>
-                    </div>
-                    <div
-                        className='w-3'
-                        style={{ backgroundColor: '#821923' }}
-                    />
-                </div>
-                <ol className='list-decimal space-y-2 px-10 lg:px-24'>
+            <div className='flex w-full flex-col gap-6'>
+                <Header>ขั้นตอนการเข้าสู่ระบบลงทะเบียน</Header>
+                <ol className='flex list-inside list-decimal flex-col gap-5 px-5'>
                     <li>
                         ให้นิสิตเข้าเว็ปไซต์{' '}
-                        <Link
-                            className='underline'
-                            href='https://www.reg.chula.ac.th'
-                            rel='noreferrer'
-                            target='_blank'
-                        >
-                            www.reg.chula.ac.th
-                        </Link>{' '}
-                        และกดเข้าสู่ระบบ
+                        <Link href='https://www2.reg.chula.ac.th/' />
                     </li>
-                    <div>
-                        <Image
-                            alt='Handbook 1'
-                            className='w-full pb-8 pt-3 lg:pb-16 lg:pt-6'
-                            src={Handbook1}
-                        />
-                    </div>
                     <li>
                         ให้นิสิตกรอกข้อมูลโดยใช้รหัสที่ได้รับจาก CU-NET
                         และใส่รหัสภาพ (captcha) และกดปุ่มตกลง
-                    </li>
-                    <div>
                         <Image
                             alt='Handbook 2'
-                            className='w-full pb-8 pt-3 lg:pb-16 lg:pt-6'
+                            className='w-full'
                             src={Handbook2}
                         />
-                    </div>
+                    </li>
                     <li>
                         ให้นิสิตเลือกเมนู “ลงปกติ” ในแถบซ้ายมือ
                         และกดปุ่มตกลงตามภาพ
-                    </li>
-                    <div>
                         <Image
                             alt='Handbook 3'
-                            className='w-full pt-3 lg:pt-6'
+                            className='w-full'
                             src={Handbook3}
                         />
-                    </div>
+                    </li>
                 </ol>
             </div>
-            <div className='w-full space-y-8 lg:space-y-10'>
-                <div className='flex h-fit w-full flex-row space-x-3'>
-                    <div
-                        className='w-fit'
-                        style={{ backgroundColor: '#821923' }}
-                    >
-                        <h2 className='px-5 py-2 text-lg font-bold text-white lg:px-10 lg:py-5 lg:text-2xl'>
-                            การลงทะเบียนเรียน
-                        </h2>
-                    </div>
-                    <div
-                        className='w-3'
-                        style={{ backgroundColor: '#821923' }}
-                    />
-                </div>
-                <ol className='list-decimal space-y-2 px-10 lg:px-24'>
-                    <li>เมื่อเข้าสู่ระบบได้แล้วจะพบหน้าต่างดังรูป</li>
-                    <div>
+            <div className='flex w-full flex-col gap-6'>
+                <Header>การลงทะเบียนเรียน</Header>
+                <ol className='flex list-inside flex-col gap-5 px-5 [counter-reset:section]'>
+                    <li className="[counter-increment:section] marker:[content:counters(section,'.')'.']">
+                        เมื่อเข้าสู่ระบบได้แล้วจะพบหน้าต่างดังรูป
                         <Image
                             alt='Handbook 4'
-                            className='w-full pb-8 pt-3 lg:pb-16 lg:pt-6'
+                            className='w-full'
                             src={Handbook4}
                         />
-                    </div>
-                    <li>ให้นิสิตใส่รหัสรายวิชาตามที่แจ้งไป</li>
-                    <div>
+                    </li>
+                    <li className="[counter-increment:section] marker:[content:counters(section,'.')'.']">
+                        ให้นิสิตใส่รหัสรายวิชาตามที่แจ้งไป
                         <Image
                             alt='Handbook 5'
-                            className='w-full pb-8 pt-3 lg:pb-16 lg:pt-6'
+                            className='w-full'
                             src={Handbook5}
                         />
-                    </div>
-                    <ol className='space-y-4 px-6 lg:px-16'>
-                        <li>
-                            2.1 สำหรับกลุ่มรายวิชา ให้นิสิตเปลี่ยนคำว่า{' '}
-                            <span
-                                className='font-bold underline'
-                                style={{ color: '#821923' }}
-                            >
-                                เท่านั้น
-                            </span>{' '}
-                            เป็น{' '}
-                            <span
-                                className='font-bold underline'
-                                style={{ color: '#821923' }}
-                            >
-                                G
-                            </span>{' '}
-                            และใส่รหัสกลุ่มรายวิชาบริเวณสีแดง <br />
-                        </li>
-                        <li>
-                            2.2 สำหรับรายวิชา EXP ENG I (5500111)
-                            ให้นิสิตกรอกตอนเรียนของตนเองหน้าช่อง{' '}
-                            <span
-                                className='font-bold underline'
-                                style={{ color: '#821923' }}
-                            >
-                                เท่านั้น
-                            </span>{' '}
-                            บริเวณสีน้ำเงินและเขียว ตามลำดับ <br />
-                        </li>
-                        <li>2.3 กดบันทึก</li>
-                    </ol>
+                        <ol className='mt-4 flex list-inside list-decimal flex-col gap-2 pl-8 [counter-reset:section]'>
+                            <li className="[counter-increment:section] marker:[content:counters(section,'.''.')]">
+                                สำหรับกลุ่มรายวิชา ให้นิสิตเปลี่ยนคำว่า{' '}
+                                <span className='text-primary font-bold underline'>
+                                    เท่านั้น
+                                </span>{' '}
+                                เป็น{' '}
+                                <span className='text-primary font-bold underline'>
+                                    G
+                                </span>{' '}
+                                และใส่รหัสกลุ่มรายวิชาบริเวณสีแดง <br />
+                            </li>
+                            <li className="[counter-increment:section] marker:[content:counters(section,'.')'.']">
+                                สำหรับรายวิชา EXP ENG I (5500111)
+                                ให้นิสิตกรอกตอนเรียนของตนเองหน้าช่อง{' '}
+                                <span className='text-primary font-semibold underline'>
+                                    เท่านั้น
+                                </span>{' '}
+                                บริเวณสีน้ำเงินและเขียว ตามลำดับ <br />
+                            </li>
+                            <li className="[counter-increment:section] marker:[content:counters(section,'.')'.']">
+                                กดบันทึก
+                            </li>
+                        </ol>
+                    </li>
                 </ol>
             </div>
         </main>
