cd0f40ed9712194974c762ddbb4f4219da7f8c28
ebonian <tanadon.santisan@gmail.com>
2024-07-14 13:36:52 +0700

    fix(or67): private students data env

--- apps/or67/src/app/api/students/[studentId]/route.ts
+++ apps/or67/src/app/api/students/[studentId]/route.ts
@@ -7,7 +7,7 @@ export const GET = async (
     { params: { studentId } }: { params: { studentId: string } }
 ): Promise<NextResponse> => {
     try {
-        const url = process.env.NEXT_PUBLIC_OR67_STUDENTS;
+        const url = process.env.OR67_STUDENTS;
         if (!url) {
             throw new Error('Invalid URL');
         }
