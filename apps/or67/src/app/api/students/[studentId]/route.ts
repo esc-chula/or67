e297660e3661d4d2398f4120da80b4e6e4a51935
ebonian <tanadon.santisan@gmail.com>
2024-07-14 13:28:31 +0700

    feat(or67): get student by id route

---
+++ apps/or67/src/app/api/students/[studentId]/route.ts
@@ -0,0 +1,52 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { Student } from '@/types/student';
+
+export const GET = async (
+    _req: NextRequest,
+    { params: { studentId } }: { params: { studentId: string } }
+): Promise<NextResponse> => {
+    try {
+        const url = process.env.NEXT_PUBLIC_OR67_STUDENTS;
+        if (!url) {
+            throw new Error('Invalid URL');
+        }
+
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error('Failed to fetch students');
+        }
+
+        const students = (await res.json()) as Student[];
+
+        const studentData = students.find(
+            (student: Student) => student.id === studentId
+        );
+
+        if (!studentData) {
+            throw new Error('Student ID not found');
+        }
+
+        return NextResponse.json(studentData);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                {
+                    error: error.message,
+                },
+                {
+                    status: 404,
+                }
+            );
+        }
+
+        return NextResponse.json(
+            {
+                error: 'Failed to fetch',
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+};
